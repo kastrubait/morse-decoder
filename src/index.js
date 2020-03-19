8b14eb8789633d31d637232b17b764dde9a3e6db
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-19 20:57:19 +0300

    add solution morse-decoder task
    add solution morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr =[];
+    for (let i = 0; i < expr.length; i +=10)
+        arr = arr.concat(expr.slice(i, i+10));
+    let newArr = [];
+    for ( let el of arr) {
+        let strR = [];
+        if (el === '**********') {
+            strR = strR + ' ';
+            newArr.push(strR);
+        } else {
+            let a ='';
+            for (let i = 0; i < el.length; i +=2) {
+                a = el.substr(i, 2)
+                switch (a) {
+                    case '00':
+                        strR = strR + '';
+                        break;
+                    case '10':
+                        strR = strR + '.';
+                        break;
+                    case '11':
+                        strR = strR + '-';
+                        break;
+                }
+            }
+            newArr = newArr.concat(MORSE_TABLE[strR]);
+        }
+    }
+    return newArr.join('');
 }
 
 module.exports = {
